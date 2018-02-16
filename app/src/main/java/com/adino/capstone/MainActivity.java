1e23d9c93ef0271dc312c149cdbdb440ef6610c2
Afadinsro <mills.josef@gmail.com>
2018-02-16 16:54:53 +0000

    Google Services and Firebase database added

--- app/src/main/java/com/adino/capstone/MainActivity.java
+++ app/src/main/java/com/adino/capstone/MainActivity.java
@@ -17,14 +17,20 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_map:
+                    mTextMessage.setText(R.string.title_map);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_trending:
+                    mTextMessage.setText(R.string.title_trending);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_capture:
+                    mTextMessage.setText(R.string.title_capture);
+                    return true;
+                case R.id.navigation_reports:
+                    mTextMessage.setText(R.string.title_reports);
+                    return true;
+                case R.id.navigation_contacts:
+                    mTextMessage.setText(R.string.title_contacts);
                     return true;
             }
             return false;
