6abf18c4a99942bbc1230c8dba42cc2392c92752
Afadinsro <mills.josef@gmail.com>
2018-02-22 15:10:31 +0000

    Renamed fragment layout files for Capture

--- app/src/main/java/com/adino/capstone/VideoFragment.java
+++ app/src/main/java/com/adino/capstone/VideoFragment.java
@@ -23,7 +23,7 @@ public class VideoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video, container, false);
+        return inflater.inflate(R.layout.fragment_capture_video, container, false);
     }
 
 }
