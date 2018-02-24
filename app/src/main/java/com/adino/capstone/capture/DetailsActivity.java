d8460627b11fa28a835cdf33ec3d46c4f7f5a5cf
Afadinsro <mills.josef@gmail.com>
2018-02-24 15:20:21 +0000

    AppGlideModule added. Dependencies for Firebase and Firebase(UI) added

---
+++ app/src/main/java/com/adino/capstone/capture/DetailsActivity.java
@@ -0,0 +1,16 @@
+package com.adino.capstone.capture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.adino.capstone.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+    }
+}
