d65b72e3fc3dc9d7ca5835a3e49db01d7df317f2
Afadinsro <mills.josef@gmail.com>
2018-02-24 16:25:23 +0000

    Created Details activity. Added some string resources

--- app/src/main/java/com/adino/capstone/capture/DetailsActivity.java
+++ app/src/main/java/com/adino/capstone/capture/DetailsActivity.java
@@ -1,16 +1,25 @@
 package com.adino.capstone.capture;
 
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.adino.capstone.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
 
     }
 }
