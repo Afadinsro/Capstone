c29496c314c84f66c8eaebcc51a2949f7b7fddfa
Afadinsro <mills.josef@gmail.com>
2018-03-12 17:57:46 +0000

    Report class fully commented.

--- app/src/main/java/com/adino/capstone/model/Report.java
+++ app/src/main/java/com/adino/capstone/model/Report.java
@@ -11,55 +11,104 @@ public class Report {
     private String imageURL;
     private String location;
 
-    public Report() {
-        // Default constructor required by Firebase
-    }
+    /**
+     * Default constructor required by Firebase
+     */
+    public Report() {}
 
+    /**
+     * Constructor
+     * @param caption caption
+     * @param date date
+     * @param category category
+     * @param imageURL image URL
+     * @param location location
+     */
     public Report(String caption, String date, String category, String imageURL, String location) {
-        this.date = date;
-        this.category = category;
-        this.caption = caption;
-        this.imageURL = imageURL;
-        this.location = location;
+        setCaption(caption);
+        setCategory(category);
+        setDate(date);
+        setImageURL(imageURL);
+        setLocation(location);
     }
     /****************************SETTER METHODS***********************************/
-    public void setCategory(String category) {
+    /**
+     * Set category
+     * @param category category
+     */
+    private void setCategory(String category) {
         this.category = category;
     }
 
-    public void setCaption(String caption) {
+    /**
+     * Set caption
+     * @param caption caption
+     */
+    private void setCaption(String caption) {
         this.caption = caption;
     }
 
-    public void setDate(String date) {
+    /**
+     * Set date
+     * @param date date
+     */
+    private void setDate(String date) {
         this.date = date;
     }
 
-    public void setImageURL(String imageURL) {
+    /**
+     * Set image URL
+     * @param imageURL image URL
+     */
+    private void setImageURL(String imageURL) {
         this.imageURL = imageURL;
     }
 
-    public void setLocation(String location) {
+    /**
+     * Set location
+     * @param location location
+     */
+    private void setLocation(String location) {
         this.location = location;
     }
 
     /****************************GETTER METHODS***********************************/
+    /**
+     * Get category
+     * @return Report category
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * Get caption
+     * @return Report acption
+     */
     public String getCaption() {
         return caption;
     }
 
+    /**
+     * Get date
+     * @return Report date
+     */
     public String getDate() {
         return date;
     }
 
+    /**
+     * Get image URL
+     * @return image URL
+     */
     public String getImageURL() {
         return imageURL;
     }
 
+    /**
+     * Get location
+     * @return location
+     */
     public String getLocation() {
         return location;
     }
